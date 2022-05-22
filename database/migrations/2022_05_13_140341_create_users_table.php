c87e1bc22793e3d9ffd7b95b0eaadfe46bef3a7b
FrancescaPalazzetti <S1094008@studenti.univpm.it>
2022-05-22 16:17:47 +0200

    create user table

--- database/migrations/2022_05_13_140341_create_users_table.php
+++ database/migrations/2022_05_13_140341_create_users_table.php
@@ -14,40 +14,29 @@ class CreateUsersTable extends Migration
     public function up()
     {
         
+        
+        
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('username',20);
-            $table->string('password');
-            //$table->enum('categoria', ['locatario','locatore','admin']);
-            $table->string('role');
-            $table->text('fotoProfilo')->nullable();
             $table->string('nome');
             $table->string('cognome');
-            $table->string('sesso',1);
+            $table->string('email');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username',20);
+            $table->string('password');
+            $table->enum('role',['locatore', 'locatario']);
+            $table->string('fotoProfilo')->nullable();
+            $table->enum('sesso', ['F', 'M']);
             $table->date('dataNascita')->nullable();
             $table->string('citta')->nullable();
             $table->string('numTelefono',15)->nullable();
-            $table->string('email');
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('universita')->nullable();
             $table->string('facolta')->nullable();
-            $table->year('annoImmatricolazione')->nullable();
+            $table->string('annoImmatricolazione')->nullable();
             $table->rememberToken();
             $table->timestamps();
-        });
-        /*
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('surname');
-            $table->string('email');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('username',20);
-            $table->string('password');
-            $table->string('role',10)->default('user');
-            $table->rememberToken();
-            $table->timestamps();                        
-        });*/
+            
+            });
     }
 
     /**
